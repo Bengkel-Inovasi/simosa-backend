b00324592c78511c9c6abcb0ade1654d6bf6be68
Dhali-YD4GXP <rozan@DhaliRozan.localdomain>
2026-06-24 08:48:33 +0700

    fix: support DATABASE_URL for Postgres connection in Railway

--- internal/database/database.go
+++ internal/database/database.go
@@ -18,13 +18,17 @@ func InitDB() {
 	dbType := os.Getenv("DB_TYPE")
 
 	if dbType == "postgres" {
-		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-			os.Getenv("DB_HOST"),
-			os.Getenv("DB_USER"),
-			os.Getenv("DB_PASSWORD"),
-			os.Getenv("DB_NAME"),
-			os.Getenv("DB_PORT"),
-		)
+		dsn := os.Getenv("DATABASE_URL")
+		if dsn == "" {
+			dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+				os.Getenv("DB_HOST"),
+				os.Getenv("DB_USER"),
+				os.Getenv("DB_PASSWORD"),
+				os.Getenv("DB_NAME"),
+				os.Getenv("DB_PORT"),
+			)
+		}
+		fmt.Println("Connecting to PostgreSQL database...")
 		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	} else {
 		// Default to SQLite for local development
